46b063bc8d31fa176521baabb8c24484cebfd08c
Nassima Ayadi <gsmicrodev@gmail.com>
2023-01-06 09:34:55 +0100

    add data base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
 import Home from "./pages/Home";
 
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App font-cinzel text-yellow-500">
       <Home />
       <p>coucou</p>
     </div>
