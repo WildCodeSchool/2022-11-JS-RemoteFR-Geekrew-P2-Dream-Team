60ad749e160b2a84e1cd730b5fff09d1c5b4e751
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-01-15 15:41:29 +0100

    fix import path navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "@components/Navbar";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Apropos from "./pages/A-Propos";
 import Catalogue from "./pages/Catalogue";
