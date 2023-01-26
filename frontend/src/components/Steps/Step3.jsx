6f3600bbf8b623c054245793c883f6165a4d1ad3
Cassandra Blondez <[cassandra.blondez@gmail.com](mailto:cassandra.blondez@gmail.com)>
2023-01-26 18:16:46 +0100

    fix index of experience and margin of the title

--- frontend/src/components/Steps/Step3.jsx
+++ frontend/src/components/Steps/Step3.jsx
@@ -18,11 +18,11 @@ function Step3({
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <h1 className="text-white font-cinzel text-2xl">
+      <h1 className="text-white font-cinzel text-2xl mb-10 pl-5 md:pl-0">
         Choisissez votre type de lieu
       </h1>
       <div
-        className={`z-50 bg-noise-pattern flex flex-col justify-center items-center border-solid border-2 border-medium-grey rounded-xl h-[32rem] w-[20rem] md:w-[40rem] ${handleBackgrounds()}`}
+        className={`z-10 bg-noise-pattern flex flex-col justify-center items-center border-solid border-2 border-medium-grey rounded-xl h-[32rem] w-[20rem] md:w-[40rem] ${handleBackgrounds()}`}
         onMouseOut={handleMouseOut}
         onBlur={() => handleMouseOut}
       >
@@ -30,7 +30,7 @@ function Step3({
           <button
             key={value}
             type="button"
-            className="z-50 flex bg-yellow justify-center py-3 my-5 md:py-4 md:my-7 rounded-full font-sans md:text-2xl text-xl text-white font-thin w-3/6"
+            className="z-10 flex bg-yellow justify-center py-3 my-5 md:py-4 md:my-7 rounded-full font-sans md:text-2xl text-xl text-white font-thin w-3/6"
             onClick={() => handleLocChange(value)}
             onMouseOver={() => handleMouseOver({ value, label })}
             onFocus={() => handleMouseOver}
